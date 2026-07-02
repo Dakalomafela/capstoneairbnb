ca484550bfab7ba3ffe435ef91897c919fedffb2
Dakalomafela <mafelainnocent@gmail.com>
2026-07-02 20:29:22 +0200

    restructure for vercel

--- frontend/src/services/api.js
+++
@@ -1,7 +0,0 @@
-﻿export const api = {
-  checkBackend: () => fetch('/api/listings', { method: 'HEAD' })
-    .then(res => res.ok)
-    .catch(() => false),
-  
-  getListings: () => fetch('/api/listings').then(res => res.json()),
-}
